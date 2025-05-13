97545846aa8f352764d7067775aaaeaa447f8efc
Eizaku <eizakuasi@gmail.com>
2025-05-13 16:30:13 -0400

    change

--- clashperk_scraper.py
+++ clashperk_scraper.py
@@ -50,6 +50,8 @@ async def fetch_rendered_html(url):
     
     if os.environ.get("deployed", "development") == "deployment":
         options = Options()
+        user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+        options.add_argument(f'user-agent={user_agent}')
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
